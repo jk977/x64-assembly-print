ab94ba38dd2b896bdf24c3b00b8821be55a705c9
jk977 <jdk64@comcast.net>
2017-09-28 15:47:48 -0700

    Minor changes

--- main.s
+++ main.s
@@ -6,8 +6,8 @@ section .text
 global main
 
 main:
-    push length ;push second parameter to stack
     push output ;push first parameter to stack
+    push length ;push second parameter to stack
     call print  ;call function - pushes return address on stack
 
     mov rax, 60 ;system exit call
@@ -15,8 +15,8 @@ main:
     syscall     ;exit
 
 print:
-    ;param1: char pointer to output (accessed via rbp+16)
-    ;param2: pointer to output length (accessed via rbp+24)
+    ;param1 (rbp+24): char pointer to output (array of bytes)
+    ;param2 (rbp+16): pointer to output length (64-bit)
 
     push rbp        ;push base pointer to stack
     mov rbp, rsp    ;stores current top of stack in base pointer
@@ -25,18 +25,18 @@ print:
     ;   Top
     ;       - Return address (rbp+0; pushed by call)
     ;       - Previous top of stack (rbp+8)
-    ;       - output (rbp+16; parameter)
-    ;       - length (rbp+24; parameter)
+    ;       - length (rbp+16; parameter)
+    ;       - output (rbp+24; parameter)
     ;   Bottom (start of unallocated memory)
 
     push rdi
     push rsi
 
-    mov r8, qword [rbp+24]  ;stores address of string length in r8
+    mov r8, qword [rbp+16]  ;stores address of string length in r8
 
     mov rax, 1              ;sys_write call
     mov rdi, 1              ;file descriptor
-    mov rsi, qword [rbp+16] ;pointer to first char in `hi!\n`
+    mov rsi, qword [rbp+24] ;pointer to first char in `hi!\n`
     mov rdx, qword [r8]     ;string length
     syscall                 ;print
 
